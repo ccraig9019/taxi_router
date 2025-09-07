fcf2fe36eac0f86989453ea48ad3845d5ebed77e
Calum <calumjamescraig@gmail.com>
2025-09-07 17:52:55 +0100

    Edited api test script

---
+++ test_google_api.py
@@ -0,0 +1,31 @@
+import requests
+
+API_KEY = "AIzaSyC4dBR4oi9TrgKew3ytE2Q8RJKYaMHZmYw"
+
+#Example: Airport to various destinations
+origin = "Edinburgh Airport"
+destinations = [
+    "12 Queen St, Edinburgh",
+    "9 King St, Edinburgh",
+    "5 Prince St, Edinburgh",
+    "20 George St, Edinburgh",
+    "1 Victoria St, Edinburgh"
+]
+
+url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+
+params = {
+    "origins:": origin,
+    "destinations": "|".join(destinations),
+    "mode": "driving",
+    "key": API_KEY
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+print("Request URL:", requests.Request('GET', url, params=params).prepare().url)
+
+print("API Response:")
+print(data)
+
