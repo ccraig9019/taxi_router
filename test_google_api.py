da93b7b2f57ebf5df2ed069514ffda1149b1050c
Calum Craig <calumjamescraig@gmail.com>
2025-09-18 10:10:34 +0100

    Change output to readable matrix of all points

--- test_google_api.py
+++ test_google_api.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.gentenv("API_KEY")
+API_KEY = os.getenv("API_KEY")
 
 #Example: Airport to various destinations
-origin = "Edinburgh Airport"
-destinations = [
+points = [
+    "Edinburgh Airport",
     "12 Queen St, Edinburgh",
     "9 King St, Edinburgh",
     "5 Prince St, Edinburgh",
@@ -18,8 +18,8 @@ destinations = [
 url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
 params = {
-    "origins:": origin,
-    "destinations": "|".join(destinations),
+    "origins": "|".join(points),
+    "destinations": "|".join(points),
     "mode": "driving",
     "key": API_KEY
 }
@@ -27,8 +27,21 @@ params = {
 response = requests.get(url, params=params)
 data = response.json()
 
-print("Request URL:", requests.Request('GET', url, params=params).prepare().url)
+#print("Request URL:", requests.Request('GET', url, params=params).prepare().url)
+print("Distance Matrix (in km):")
+header = ["From/To"] + points
+print("{:<25}".format(header[0]), end="")
+for h in header[1:]:
+    print("{:<25}".format(h), end="")
+print()
 
-print("API Response:")
-print(data)
-
+#print each row
+for i, row in enumerate(data["rows"]):
+    print("{:<25}".format(points[i]), end="")
+    for elem in row["elements"]:
+        if elem["status"] == "OK":
+            dist_km = elem["distance"]["value"] / 1000  # convert meters to km
+            print("{:<25}".format(f"{dist_km:.1f} km"), end="")
+        else:
+            print("{:<25}".format("N/A"), end="")
+    print()
